87b6d11f7889fa1c4f43fe57f8c58dcf1d84007b
kw-co <aaa111aaa111@gmail.com>
2026-08-09 18:57:33 +0300

    ل

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: command === 'build' ? '/pit/' : '/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
